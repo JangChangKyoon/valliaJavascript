4068b1ed0da99270abf6e329910f5c789fc202df
JangChangKyoon <58103130+JangChangKyoon@users.noreply.github.com>
2022-11-06 21:00:37 +0900

    add geo and whether

--- js/background.js
+++ js/background.js
@@ -1,15 +1,15 @@
 //배경 이미지들 변수에 저장하기
-const images = ['wooyoungwoo1.jpg', 'wooyoungwoo2.jpg', 'wooyoungwoo3.jpg'];
+const images = ["wooyoungwoo1.jpg", "wooyoungwoo2.jpg", "wooyoungwoo3.jpg"];
 
 //배경 이미지 선택하여 변수에 저장하기
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 //배경 이미지에 넣은 element 생성하기
-const bgImage = document.createElement('img');
+const bgImage = document.createElement("img");
 //=> <img />
 
 //배경 이미지에 넣은 element 안 내용물 구성하기
 bgImage.src = `img/${chosenImage}`;
 //=>  <img src="img/chosenImage"/>
 
-document.body.appendChild(bgImage);
+document.querySelector(".pic").appendChild(bgImage);
